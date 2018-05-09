10bbabc10db918fbfebec930aed352002906a047
Gordon Trang <trangg@amazon.com>
2018-05-09 06:39:17 -0700

    Naively attack using la raza

--- src/squaregame/squares/laraza/DefaultSquare.java
+++ src/squaregame/squares/laraza/DefaultSquare.java
@@ -7,77 +7,37 @@ import squaregame.model.SquareAction;
 import squaregame.squares.SquareLogic;
 import squaregame.utils.SquareLogicUtilities;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
 
 /**
- * Created by Russell on 5/5/18.
+ * Greedy attacker.
  */
 public class DefaultSquare extends SquareLogic {
 
-    private static class Dimension {
-        private int row;
-        private int col;
-
-        private Dimension(int row, int col) {
-            this.row = row;
-            this.col = col;
-        }
-
-        public int hashCode() {
-            return this.row * 1000 + this.col;
-        }
-
-        public String toString() {
-            return String.format("%s,%s ", this.row, this.col);
-        }
-    }
-
-    private static Set<Dimension> current = new HashSet<>();
-    private static Set<Dimension> previous = new HashSet<>();
-
-    private static boolean FIRST_RUN = true;
-    private static int INITIAL_ROW;
-    private static int INITIAL_COL;
-    private static Dimension lastDim = new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE);
-
     @Override
     public SquareAction run(MagicSquare magicSquare, int row, int col, List<Player> view) {
-        if (FIRST_RUN) {
-            INITIAL_ROW = row;
-            INITIAL_COL = col;
-            FIRST_RUN = false;
+
+        // Kill if possible
+        final List<Direction> attackDirections = SquareLogicUtilities.getEnemyDirections(view, magicSquare.getPlayer());
+        if (!attackDirections.isEmpty()) {
+            Direction direction = attackDirections.get(ThreadLocalRandom.current().nextInt(attackDirections.size()));
+            return SquareAction.attack(direction, this);
         }
 
-        boolean newRound = row <= lastDim.row && col < lastDim.col;
-        if (newRound) {
-            previous = current;
-            current = new HashSet<>();
+        // Replicate if not possible
+        final List<Direction> replicateDirections = SquareLogicUtilities.getEmptyDirections(view);
+        if (!replicateDirections.isEmpty()) {
+            Direction direction = replicateDirections.get(ThreadLocalRandom.current().nextInt(replicateDirections.size()));
+            return SquareAction.replicate(direction, this, new DefaultSquare());
         }
 
-        lastDim.row = row;
-        lastDim.col = col;
-
-        current.add(new Dimension(row, col));
-
-        final List<Direction> directions = SquareLogicUtilities.getEmptyDirections(view).stream()
-                .filter(direction -> !current.contains(new Dimension(row + direction.getxOffset(), col + direction.getyOffset())))
-                .filter(direction -> !previous.contains(new Dimension(row + direction.getxOffset(), col + direction.getyOffset())))
-                .collect(Collectors.toList());
-        if (directions.isEmpty()) {
-            return SquareAction.wait(this);
-        }
-
-        Direction direction = directions.get(ThreadLocalRandom.current().nextInt(directions.size()));
-        current.add(new Dimension(row + direction.getxOffset(), col + direction.getyOffset()));
-        return SquareAction.replicate(direction, this, new DefaultSquare());
+        // Nothing to kill or replicate to, hold position
+        return SquareAction.wait(this);
     }
 
     @Override
     public String getSquareName() {
-        return "la raza";
+        return "La Raza";
     }
 }
